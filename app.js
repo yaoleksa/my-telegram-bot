584eba55c965072ae19ef0219f39e24ae3c084cb
Oleksii <oleksii.y@peeklogic.com>
2023-04-01 18:30:27 +0300

    improve my bot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // all packages init
 const express = require('express');
 const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
 const axios = require('axios');
 
 // define port
@@ -15,12 +16,48 @@ app.use(express.static('static'));
 // create bot
 const tb = new Telegraf('5920614032:AAHP9b8SiOpOGoTD9I1RZBBOeVyuqllGZHk');
 
-//define start command
+// define start command
 tb.command('start', ctx => {
-    tb.telegram.sendMessage(ctx.chat.id, 
-        'Привіт! Я створений для того, щоб говорити тобі якісь пафосні цитати і цікаві факти про котів\n' + 
-        'Я нещодавно в Україні, цій прекрасній країні, тому ще не встиг досконало вивчити українську\n' + 
-        'Тому, на жаль, я буду писати тобі англійською');
+    ctx.reply('Привіт! Я створений для того, щоб говорити тобі якісь пафосні цитати\n' + 
+    'і цікаві факти про котів' +
+    'Я нещодавно в Україні, цій прекрасній країні, тому ще не встиг досконало вивчити українську\n' +
+    'Тому, на жаль, я буду писати тобі англійською');
+});
+
+// Fetch interesting answer
+let catsFact;
+axios.get('https://catfact.ninja/fact').then(respons => {
+    catsFact = respons.data.fact;
+});
+let q;
+axios.get('https://quotes15.p.rapidapi.com/quotes/random/', {
+    headers: {
+        'X-RapidAPI-Key': 'dc40d2b288msh88ced99c0191b37p144f83jsne853bb67a11f',
+        'X-RapidAPIHost': 'quotes15.p.rapidapi.com'
+    }
+}).then(respons => {
+    q = respons.data.content;
+});
+
+// define answer
+let answer;
+
+// defeine general answer
+
+tb.hears(/[a-zA-Z0-9]/, ctx => {
+    answer = parseInt(Math.random() * 2) > 0 ? catsFact : q;
+    ctx.reply(answer);
+    axios.get('https://catfact.ninja/fact').then(respons => {
+        catsFact = respons.data.fact;
+    });
+    axios.get('https://quotes15.p.rapidapi.com/quotes/random/', {
+    headers: {
+        'X-RapidAPI-Key': 'dc40d2b288msh88ced99c0191b37p144f83jsne853bb67a11f',
+        'X-RapidAPIHost': 'quotes15.p.rapidapi.com'
+    }
+    }).then(respons => {
+        q = respons.data.content;
+    });
 });
 
 // define GET server handler
